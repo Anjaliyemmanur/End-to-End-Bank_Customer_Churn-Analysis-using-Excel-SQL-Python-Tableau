196a47866d4d07b70f3459217f5260152af171be
Anjali <Anjaliyemmanur@gmail.com>
2026-02-05 15:31:19 +0530

    Update 03_diagnostic_analysis.sql

--- sql/03_diagnostic_analysis.sql
+++ sql/03_diagnostic_analysis.sql
@@ -1,1 +1,20 @@
-
+️## Diagnostic Analysis(Why is churn happening?)
+  
+Behavioral Differences
+1.	How do churned vs non-churned customers differ in terms of:
+o	Average balance
+o	Average tenure
+o	Average credit score
+o	Average number of products
+Account Activity & Engagement
+1.	Does being an active member reduce churn?
+2.	Is there a difference in churn between customers who:
+o	Have a credit card vs don’t
+o	Have 1 product vs multiple products
+Geography-Specific Behavior
+1.	How does customer behavior differ across France, Germany, and Spain?
+2.	Why does churn appear higher in certain geographies?
+3.	Are high-balance customers churning more in specific regions?
+Tenure & Loyalty
+1.	How does tenure impact churn probability?
+2.	At which tenure range does churn peak?
